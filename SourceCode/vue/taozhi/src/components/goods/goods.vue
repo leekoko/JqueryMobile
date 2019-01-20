f8781acc0ca2d55e72b1fe82e77198ce64a33f03
liyb <908230189@qq.com>
2019-01-20 23:39:22 +0800

    添加内容展示效果

--- SourceCode/vue/taozhi/src/components/goods/goods.vue
+++ SourceCode/vue/taozhi/src/components/goods/goods.vue
@@ -1,5 +1,25 @@
 <template>
-  <h1>goods内容</h1>
+  <div class="content">
+    <img src="https://fiverr-res.cloudinary.com/t_medium5_x2,q_auto,f_auto/gigs/22708572/original/a08fabfee405a8532f520e5d7fb82b64b132bf0b.jpg" style="width: 100%;height:175px; "></img>
+    <div class="seller-info">
+        <span style="margin: 10px;float: left;">
+          <img style="border-radius:50%;" src="https://fiverr-res.cloudinary.com/t_profile_thumb,q_auto,f_auto/profile/photos/3674546/original/1429240227873_Profile.png">
+        </span>
+      <span style="float: left;margin-top:15px;">
+          <a href="">sukunj</a><br/>
+          <span style="font-size: 16px;color: dimgrey;">Level 2 Seller</span>
+        </span>
+      <span style="float: left;margin:15px;width: 90%; height: 130px;border-bottom: 1px solid rgba(105,105,105,0.15)">
+          <h3><a href="" style="color: black;">I will web programmer php, wordpress , open cart developer</a></h3>
+        </span>
+      <span>
+          <h3 style="float: right;margin-right: 20px;">Starting At<h2 style="float: right;margin-left: 3px;">￥100</h2></h3>
+
+
+
+        </span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +29,13 @@ export default {
 </script>
 
 <style scoped>
-
+.content{
+  background:white;
+  width: 300px;
+  height: 450px;
+  border: 1px solid rgba(105,105,105,0.15);
+  float: left;
+  margin-left: 30px;
+  margin-top: 30px;
+}
 </style>
--- SourceCode/vue/taozhi/src/view/home.vue
+++ SourceCode/vue/taozhi/src/view/home.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="home">
+  <div class="home" style="margin-top: 100px;margin-left: 50px;">
     <!--Inspired By Services You’ve Viewed-->
+    <h1>Inspired By Services You’ve Viewed</h1>
+
+    <!--展示商品块start-->
+    <v-goods></v-goods>
+    <!--展示商品块end-->
 
     <!--Explore The Marketplace-->
 
     <!--Get Work Done Faster On Fiverr, With Confidence-->
 
-    <div class="tab">
+<!--    <div class="tab">
       <div class="tab-item">
         <router-link to="/goods">商品1</router-link>
       </div>
@@ -17,15 +22,20 @@
         <router-link to="/seller">商家</router-link>
       </div>
     </div>
-    <router-view></router-view>
-    <i-button @click="show">Click me!</i-button>
-    <Modal v-model="visible" title="Welcome">Welcome to iView</Modal>
+    <router-view></router-view>-->
+<!--    <i-button @click="show">Click me!</i-button>
+    <Modal v-model="visible" title="Welcome">Welcome to iView</Modal>-->
   </div>
 </template>
 
 <script>
+  import goods from '../components/goods/goods.vue'
+
     export default {
       name: "home",
+      components: {
+        'v-goods': goods,
+      },
       data() {
         return{
           visible: false
